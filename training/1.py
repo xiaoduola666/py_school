dae1d65911b20edab8d4b1352bce6bfb74fe0c66
wutianle <wutianle@shihuo.com>
2020-11-16 18:29:16 +0800

    1

---
+++ training/1.py
@@ -0,0 +1,21 @@
+# 2020年11月16日18:05:43  数据类型和变量
+# 请打印出以下变量的值：
+# n = 123
+# f = 456.789
+# s1 = 'Hello, world'
+# s2 = 'Hello, \'Adam\''
+# s3 = r'Hello, "Bart"'
+# s4 = r'''Hello,
+# Lisa!'''
+
+
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print(s4)
+
+# 笔记： 全部是单引号的用转移符号来打印
