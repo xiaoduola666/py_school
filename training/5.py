8d0f615321879768fcdc2dcf4674414072ad2150
wutianle <wutianle@shihuo.com>
2020-11-17 10:40:18 +0800

    5

---
+++ training/5.py
@@ -0,0 +1,12 @@
+# 2020年11月17日10:36:45   循环
+
+# 请利用循环依次对list中的每个名字打印出Hello, xxx!：
+# L = ['Bart', 'Lisa', 'Adam']
+# for i in L:
+#     print('hello', i)
+
+
+#笔记
+# 1. break 可以提前跳出循环体
+# 2. continue 可以跳出本次循环
+# 3.循环只有while  和  for
